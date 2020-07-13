b60ac3f669fdc15ef42c05386a01aeb9769171d5
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2020-07-13 19:49:55 -0300

    Implementing GET /?period=yyyy-mm route

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,4 +1,22 @@
 const express = require('express');
 const transactionRouter = express.Router();
+const TransactionModel = require('../models/TransactionModel');
+
+transactionRouter.get('/', async (req, res) => {
+  try {
+    const urlParams = new URLSearchParams(req.query)
+    const period = urlParams.get('period')
+    let transactions
+    if(period) {
+      transactions = await TransactionModel.find({yearMonth: period})
+    } else {
+      res.status(400).send('GET must have a valid /:period in yyyy-mm format')
+    }
+    if(transactions)
+      res.status(200).send(transactions)
+  } catch (error) {
+    res.status(501).send('GET /?period=yyyy-mm Fail! ' + error)
+  }
+})
 
 module.exports = transactionRouter;
